ceb421ee2e7e596f5a49440b01cc8c05661adf9f
Umberto <umbertopellegrino96@gmail.com>
2020-07-13 11:41:14 +0200

    keybordEnter

--- js/jscript.js
+++ js/jscript.js
@@ -1,19 +1,32 @@
 function writeMes(){
-  var mess;
-  $("#inviaMes").click(function () {
-    mess=$("#messaggio").val();
-    console.log(mess);
-    $("#chat").append('<div class="user">' + mess +'</div>');
-    $("#messaggio").val("");
-    setTimeout(function () {
-      $("#chat").append('<div class="answer"> ok! </div>');
-    }, 1000);
-  });
 
+  $("#inviaMes").click(sendMessage);
+
+}
+
+function keyBordEnter() {
+
+  $("#messaggio").keydown(function(){
+    if (event.which == 13) {
+      sendMessage()
+    }
+  });
+}
+
+function sendMessage() {
+  var mess;
+  mess=$("#messaggio").val();
+  console.log(mess);
+  $("#chat").append('<div class="user">' + mess +'</div>');
+  $("#messaggio").val("");
+  setTimeout(function () {
+    $("#chat").append('<div class="answer"> ok! </div>');
+  }, 1000);
 }
 
 function init() {
   writeMes()
+  keyBordEnter()
 }
 
 $(document).ready(init);

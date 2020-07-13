84c97f16d481b1dfbac4b1407b861149cb79861a
Umberto <umbertopellegrino96@gmail.com>
2020-07-13 11:27:26 +0200

    Invio mess con button

--- js/jscript.js
+++ js/jscript.js
@@ -1,1 +1,15 @@
+function takeMes(){
+  var mess;
+  $("#inviaMes").click(function () {
+    mess=$("#messaggio").val();
+    console.log(mess);
+    $("#chat").append('<div class="user">' + mess +'</div>');
+  });
 
+}
+
+function init() {
+  takeMes()
+}
+
+$(document).ready(init);

345f701669e0a23d119fe5a5f0820fdbdc67a25d
Umberto <umbertopellegrino96@gmail.com>
2020-07-13 11:32:06 +0200

    Answer

--- js/jscript.js
+++ js/jscript.js
@@ -1,15 +1,19 @@
-function takeMes(){
+function writeMes(){
   var mess;
   $("#inviaMes").click(function () {
     mess=$("#messaggio").val();
     console.log(mess);
     $("#chat").append('<div class="user">' + mess +'</div>');
+    $("#messaggio").val("");
+    setTimeout(function () {
+      $("#chat").append('<div class="answer"> ok! </div>');
+    }, 1000);
   });
 
 }
 
 function init() {
-  takeMes()
+  writeMes()
 }
 
 $(document).ready(init);
